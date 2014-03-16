b3b9a2ae8dc602dddfb6c17643c1e32625a66644
Gavin Simpson <ucfagls@gmail.com>
2014-03-16 13:10:11 -0600

    sim2dpoisson can return expectations instead of random counts

--- R/poisson-sim.R
+++ R/poisson-sim.R
@@ -62,6 +62,16 @@
 ##' species tolerance, and \eqn{h}, the height of the response curve
 ##' at the optimum.
 ##'
+##' If \code{expectation = TRUE} the mean response for the parameters
+##' is returned. If \code{expectation = FALSE} counts are drawn randomly
+##' from a Poisson distribution with mean (argument \code{lamba}) given
+##' by the Gaussian response.
+##'
+##' @section Note:
+##' When called with \code{expectation = FALSE} the function does not use
+##' the pseudorandom number generator, but when called with the defaults
+##' a single call to pseudorandom number generator is made.
+##'
 ##' @title Simulate Poisson counts along two, possibly correlated gradients
 ##' @param x1 numeric; locations along gradient 1.
 ##' @param x2 numeric; locations along gradient 2.
@@ -71,6 +81,8 @@
 ##' @param opt2 numeric; species optima on gradient 2, one per taxon.
 ##' @param tol2 numeric; species tolerance on gradient 2, one per taxon.
 ##' @param corr numeric; the correlation between gradients.
+##' @param expectation  logical; should expectations (mean response) be
+##' returned?
 ##'
 ##' @return A matrix of simulate counts.
 ##'
@@ -92,7 +104,7 @@
 ##' y <- sim2dPoisson(x1, x2, Opt1, Opt2, Tol1, Tol2, H, corr = 0.5)
 ##'
 `sim2dPoisson` <- function(x1, x2, opt1, tol1, h, opt2, tol2,
-                           corr = 0) {
+                           corr = 0, expectation = FALSE) {
     stopifnot(isTRUE(all.equal(n1 <- length(x1), n2 <- length(x2))))
     ex1 <- expandGauss(x1, opt1, tol1, h)
     ex2 <- expandGauss(x2, opt2, tol2, h)[, -4] ## don't need extra h
@@ -103,7 +115,11 @@
                              opt1 = ex1[, "opt1"], tol1 = ex1[, "tol1"],
                              opt2 = ex2[, "opt2"], tol2 = ex2[, "tol2"],
                              h = ex1[, "h"], corr = corr)
-    sim <- rpois(n1, mu)
+
+    if (expectation)
+        sim <- mu
+    else
+        sim <- rpois(n1, mu)
     sim <- matrix(sim, nrow = n1)
     sim
 }
