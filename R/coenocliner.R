b3bc1c2c70738e31132d769181b38a1a5962c99a
Gavin Simpson <ucfagls@gmail.com>
2014-07-22 20:01:47 -0600

    Adds a coenocliner-package help file. Completes issue #12.

---
+++ R/coenocliner.R
@@ -0,0 +1,25 @@
+##' @title A coenocline simulation package for R
+##'
+##' @description \pkg{coenocliner} provides a simple, easy interface for simulating species abundance (counts) or occurrence along gradients.
+##'
+##' @details One of the key ways quantitative ecologists attempt to understand the properties and behaviour of the methods they use or dream up is through the use of simulated data. \pkg{coenocliner} is an R package that provides a simple interface to coenocline simulation.
+##'
+##' Species data can be simulated from a number of species response models
+##' \itemize{
+##'   \item Gaussian response
+##'   \item Generalised Beta response
+##' }
+##' and random count or occurrence data can be simulated from suitably parameterised response models by using the output from the response model as the mean or expectation of one of a number of statistical distributions
+##' \itemize{
+##'   \item Poisson
+##'   \item Negative Binomial
+##'   \item Binomial
+##' }
+##' from which random draws are made.
+##'
+##' @author Gavin L. Simpson
+##' @docType package
+##' @name coenocliner
+##' @aliases coenocliner-package
+##' @keywords package
+NULL
