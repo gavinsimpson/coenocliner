0236b55706c1b245244b83131fd1fcbd667da46e
Gavin Simpson <ucfagls@gmail.com>
2014-07-22 22:07:36 -0600

    add examples to species-response; add @keywords tag.

--- R/response-functions.R
+++ R/response-functions.R
@@ -38,6 +38,41 @@
 ##' @rdname species-response
 ##' @name species-response
 ##' @export
+##'
+##' @keywords datagen
+##'
+##' @examples
+##'
+##' # A simple example with a single species
+##' x <- seq(from = 4, to = 6, length = 100)
+##' px <- list(opt = 4.5, tol = 0.25, h = 20)
+##' G <- Gaussian(x, px = px)
+##' head(G)
+##' length(G)
+##'
+##' # A more complex example with 6 species, which needs the parameters
+##' # repeating for each gradient location:
+##'
+##' # Recreate Fig. 2 of Minchin (1987)
+##' # Parameters for each of 6 six species
+##' A0 <- c(5,4,7,5,9,8) * 10
+##' m <- c(25,85,10,60,45,60)
+##' r <- c(3,3,4,4,6,5) * 10
+##' alpha <- c(0.1,1,2,4,1.5,1)
+##' gamma <- c(0.1,1,2,4,0.5,4)
+##' # Gradient locations
+##' x <- 1:100
+##'
+##' # expand parameter set
+##' pars <- expand(x, m = m, A0 = A0, r = r, alpha = alpha,
+##'                gamma = gamma)
+##' head(pars)
+##'
+##' xvec <- pars[, "x"]
+##' px <- as.list(data.frame(pars[, -1]))
+##' spprc <- Beta(xvec, px = px)
+##' matplot(matrix(spprc, ncol = 6), ## 6 species
+##'         type = "l", lty = "solid")
 `Gaussian` <- function(x, y = NULL, px, py = NULL, corr = NULL) {
     pars <- c("opt", "tol", "h")
     sim <- if (is.null(y)) {
