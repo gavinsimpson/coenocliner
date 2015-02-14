a445cf710800b5fd4c2dae3dc2b256b6c0579d63
Gavin Simpson <ucfagls@gmail.com>
2015-02-14 13:06:53 -0600

    adds prettyHead, a tbl_df-inspired pretty printing utility for data frames/matrices

--- R/utils.R
+++ R/utils.R
@@ -73,6 +73,103 @@
     out
 }
 
+##' @title Pretty Display of a Matrix or Data Frame
+##'
+##' @description Pretty display of the first \code{n} rows of a data frame or matrix-like object, with variables/columns that cannot fit on a single screen width removed.
+##' @param x an R object for which \code{\link{head}} and \code{\link{as.data.frame}} methods exist.
+##' @param n numeric; the number of rows to display
+##' @param width numeric; the display width to assume when formatting the data frame. The default is given by \code{getOption("width")}
+##' @return A \code{\link{format}}-ed data frame with \code{n} rows (or \code{n+1} rows if only a subset is shown) and as many columns/components that can be printed on a single screen width.
+##' @author Gavin L. Simpson
+##'
+##' @rdname coenocliner-internal
+##' @keywords internal
+##' @export
+##'
+##' @examples
+##'
+##' x <- seq(from = 4, to = 6, length = 30)
+##' opt <- seq(4, 7, length = 100)
+##' tol <- rep(0.25, 100)
+##' h <- rep(20, 100)
+##' 
+##' ## simulate
+##' set.seed(1)
+##' y <- coenocline(x, responseModel = "gaussian",
+##'                 params = cbind(opt = opt, tol = tol, h = h),
+##'                 countModel = "poisson")
+##' 
+##' prettyHead(y)
+##'
+`prettyHead` <- function(x, n = 10, width = getOption("width")) {
+    ## some meta data
+    nr <- NROW(x)
+    nc <- NCOL(x)
+    ## truncate x to n rows using appropriate head() method
+    df <- as.data.frame(head(x, n = n))
+    ## nullify rownames just incase there are any (shouldn't be...)
+    rownames(df) <- NULL
+    ## add some variable names; should make this user-configurable TODO
+    colnames(df) <- paste0("Sp", seq_len(nc))
+
+    ## format df converts everything to characters but stays in data
+    ## frame format
+    fdf <- format(df)
+    ## this is a vector of character strings that represents
+    ## what will get printed on a single line
+    strings <- c(format(rownames(fdf))[[1]], unlist(fdf[1, ]))
+    ## account for the rownames, hence initial empty string
+    names <- format(c("", colnames(df)))
+    ## take length of elements of strings or names, whichever is largest
+    ## these are the widths required to display each column of fdf
+    widths <- pmax(nchar(strings), nchar(names))
+    ## cumulative sum is total width up to & including the nth column
+    cum <- cumsum(widths)
+    ## and we find which of these exceeds the current display width
+    overfl <- cum[-1] > width           # ignore rownames
+
+    if (all(overfl)) {                  # sanity check, always show 1 column
+        overfl[1] <- FALSE
+    }
+    ## should need the above unless /very/ small width with huge counts
+
+    ## loose columns we can't show in a single screen width
+    trimDF <- fdf[, !overfl, drop = FALSE]
+
+    ## dplyr adds continuation ellipsis to each column
+    ## as counts tend to have narrower columns than dplyrs use-case
+    ## add ":" as continuation chars in spirit of TeX \vdots
+    ellipsis <- nr > n
+    if (ellipsis) {
+        dots <- rep(":", ncol(trimDF))
+        trimDF <- rbind(trimDF, ":" = dots)
+    }
+
+    ## record number of species omitted
+    omitted <- if (any(overfl)) {
+        sum(overfl)
+    } else {
+        0
+    }
+
+    ## return object
+    structure(list(df = trimDF, omitted = omitted),
+              class = "prettyHead")
+}
+
+##' @export
+`print.prettyHead` <- function(x, ...) {
+    cat("\n")
+    print(x$df, ...)
+    cat("\n")
+    if (x$omitted > 0) {
+        writeLines(strwrap(paste("Counts for", x$omitted, "species not shown."),
+                           prefix = "\n"))
+        cat("\n")
+    }
+    invisible(x)
+}
+
 ##' @title Return the First of Last Part of an Object
 ##'
 ##' @description Returns the first of last \code{n} rows of the simulated counts.
