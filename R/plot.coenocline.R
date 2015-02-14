057928cc9105feedc9574fbcb21aba65b93e0d27
Gavin Simpson <ucfagls@gmail.com>
2015-02-14 09:20:41 -0600

    plot and lines methods now use the locations extractor

--- R/plot.coenocline.R
+++ R/plot.coenocline.R
@@ -16,7 +16,7 @@
 ##'
 ##' @importFrom graphics plot matplot
 `plot.coenocline` <- function(x, type = "p", ...) {
-    matplot(attr(x, "locations"), x, type = type, ...)
+    matplot(locations(x), x, type = type, ...)
 }
 
 ##' @export
@@ -24,5 +24,5 @@
 ##'
 ##' @importFrom graphics lines matlines
 `lines.coenocline` <- function(x, ...) {
-    matlines(attr(x, "locations"), x, ...)
+    matlines(locations(x), x, ...)
 }
