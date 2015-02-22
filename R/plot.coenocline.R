5de3246c3b5a1a8622fbf02133a9b7b4252fabd6
Gavin Simpson <ucfagls@gmail.com>
2015-02-22 09:52:37 -0600

    sort gradient locations and counts at these locations before plotting

--- R/plot.coenocline.R
+++ R/plot.coenocline.R
@@ -15,8 +15,32 @@
 ##' @keywords hplot
 ##'
 ##' @importFrom graphics plot matplot
+##'
+##' @examples
+##' ## Poisson counts along a single gradient, Gaussian response
+##' ## =========================================================
+##'
+##' x <- seq(from = 4, to = 6, length = 100)
+##' opt <- c(3.75, 4, 4.55, 5, 5.5) + 0.5
+##' tol <- rep(0.25, 5)
+##' h <- rep(20, 5)
+##'
+##' ## simulate
+##' set.seed(1)
+##' y <- coenocline(x, responseModel = "gaussian",
+##'                 params = cbind(opt = opt, tol = tol, h = h),
+##'                 countModel = "poisson")
+##' head(y)
+##'
+##' y <- coenocline(x, responseModel = "gaussian",
+##'                 params = cbind(opt = opt, tol = tol, h = h),
+##'                 countModel = "poisson",
+##'                 expectation = TRUE)
+##' plot(y, type = "l", lty = "solid")
 `plot.coenocline` <- function(x, type = "p", ...) {
-    matplot(locations(x), x, type = type, ...)
+    locs <- locations(x)
+    ord <- order(locs)
+    matplot(locs[ord], x[ord, ], type = type, ...)
 }
 
 ##' @export
@@ -24,5 +48,7 @@
 ##'
 ##' @importFrom graphics lines matlines
 `lines.coenocline` <- function(x, ...) {
-    matlines(locations(x), x, ...)
+    locs <- locations(x)
+    ord <- order(locs)
+    matlines(locs[ord], x[ord, ], ...)
 }
