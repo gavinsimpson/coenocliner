4e60871ab239f0fab9d075e93dc48782a6b648a0
Gavin L. Simpson <ucfagls@gmail.com>
2016-05-12 11:48:24 -0600

    replicate alpha to correct length (ie.e of mu); document that vector alpha is allowed.

--- R/distributions.R
+++ R/distributions.R
@@ -7,7 +7,7 @@
 ##'
 ##' @param n the number of random draws, equal to number of species times the number of gradient locations.
 ##' @param mu the mean or expectation of the distribution. For \code{Bernoulli}, \code{Binomial}, and \code{BetaBinomial()} this is the probability of occurrence as given by the response function.
-##' @param alpha numeric; dispersion parameter for the negative binomial distribution. The NB2 parametrization of the negative binomial is used here, in which \eqn{\alpha} is positively related to the amount of extra dispersion in the simulated data. As such, where \eqn{\alpha = 0}, we would have a Poisson distribution. \code{alpha} can be supplied a value of \code{0}, in which case \code{NegBin} and \code{ZINB} return random draws from the Poisson or zero-inflated Poisson distributions, respectively. Negative values of \code{alpha} are not allowed and will generate an error.
+##' @param alpha numeric; dispersion parameter for the negative binomial distribution. May be a vector of length \code{length(mu)}. The NB2 parametrization of the negative binomial is used here, in which \eqn{\alpha} is positively related to the amount of extra dispersion in the simulated data. As such, where \eqn{\alpha = 0}, we would have a Poisson distribution. \code{alpha} can be supplied a value of \code{0}, in which case \code{NegBin} and \code{ZINB} return random draws from the Poisson or zero-inflated Poisson distributions, respectively. Negative values of \code{alpha} are not allowed and will generate an error.
 ##'
 ##' @return a vector of random draws from the stated distribution.
 ##'
@@ -24,6 +24,7 @@
     if (any(alpha < 0L)) {
         stop("Negative values of 'alpha' are not supported")
     }
+    alpha <- rep(alpha, length.out = length(mu))
     ind <- alpha > 0L
     mu[ind] <- mu[ind] * rgamma(sum(ind), shape = 1/alpha[ind], rate = 1/alpha[ind])
     rpois(n, lambda = mu)
@@ -81,6 +82,7 @@
     if (any(alpha < 0L)) {
         stop("Negative values of 'alpha' are not supported")
     }
+    alpha <- rep(alpha, length.out = length(mu))
     ind <- alpha > 0L
     mu[ind] <- mu[ind] * rgamma(sum(ind), shape = 1/alpha[ind], rate = 1/alpha[ind])
     ifelse(runif(n) > zprobs,
